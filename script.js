a982eacf3eb3194b2a753e0b7e217840b3c89353
Leon <60196035+rausgetappt@users.noreply.github.com>
2024-06-13 21:07:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ function update() {
 }
 
 update();
+
+document.querySelectorAll("input[type='checkbox']").forEach((element) => {element.click();});
+
+function saveCheckboxStates() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      localStorage.setItem(checkbox.id, checkbox.checked);
+    });
+  }
+  
+  function loadCheckboxStates() {
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach((checkbox) => {
+      const savedState = localStorage.getItem(checkbox.id);
+      if (savedState !== null) {
+        checkbox.checked = savedState === 'true';
+      }
+    });
+  }
+  
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+  checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', saveCheckboxStates);
+  });
+  
+  document.addEventListener('DOMContentLoaded', loadCheckboxStates);
